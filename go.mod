84fd71b8ce3e4a2049035e275f8e4e9d610084e7
Victoria Mihell-Hale <victory_star@hotmail.com>
2021-09-09 16:32:24 +0100

    Try out github.com/dhowden/tag for getting metadata

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com/rank-my-music
 
 go 1.14
+
+require github.com/dhowden/tag v0.0.0-20201120070457-d52dcb253c63
--- main.go
+++ main.go
@@ -2,50 +2,75 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"os"
 
-	"example.com/rank-my-music/elo"
-	"example.com/rank-my-music/track"
+	"github.com/dhowden/tag"
 )
 
+// func main() {
+// 	tracks := [...]track.Track{
+// 		track.New("Crucify"),
+// 		track.New("Girl"),
+// 		track.New("Silent All These Years"),
+// 		track.New("Precious Things"),
+// 		track.New("Winter"),
+// 		track.New("Happy Phantom"),
+// 		track.New("China"),
+// 		track.New("Leather"),
+// 		track.New("Mother"),
+// 		track.New("Tear in Your Hand"),
+// 		track.New("Me and a Gun"),
+// 		track.New("Little Earthquakes"),
+// 	}
+
+// 	fmt.Printf("%v\n", tracks)
+
+// 	for i := 1; i <= 1000; i++ {
+// 		trackA := &tracks[rand.Intn(len(tracks))]
+// 		trackB := &tracks[rand.Intn(len(tracks))]
+
+// 		// Make B always win. Just want to see how Elo works for now.
+// 		trackA.Ranking, trackB.Ranking = elo.CalculateNewRankings(
+// 			elo.Elo{
+// 				CurrentRanking: trackA.Ranking,
+// 				Score:          0,
+// 			},
+// 			elo.Elo{
+// 				CurrentRanking: trackB.Ranking,
+// 				Score:          1,
+// 			},
+// 		)
+
+// 		fmt.Println(trackA)
+// 		fmt.Println(trackB)
+// 		fmt.Println()
+// 	}
+
+// 	fmt.Printf("%v\n", tracks)
+// }
+
 func main() {
-	tracks := [...]track.Track{
-		track.New("Crucify"),
-		track.New("Girl"),
-		track.New("Silent All These Years"),
-		track.New("Precious Things"),
-		track.New("Winter"),
-		track.New("Happy Phantom"),
-		track.New("China"),
-		track.New("Leather"),
-		track.New("Mother"),
-		track.New("Tear in Your Hand"),
-		track.New("Me and a Gun"),
-		track.New("Little Earthquakes"),
+	file, err := os.Open("/media/vmihell-hale/Arch/home/vmihell-hale/Music/Tori Amos/Little Earthquakes/01 Crucify.mp3")
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Printf("%v\n", tracks)
+	fmt.Println(file.Name())
 
-	for i := 1; i <= 1000; i++ {
-		trackA := &tracks[rand.Intn(len(tracks))]
-		trackB := &tracks[rand.Intn(len(tracks))]
-
-		// Make B always win. Just want to see how Elo works for now.
-		trackA.Ranking, trackB.Ranking = elo.CalculateNewRankings(
-			elo.Elo{
-				CurrentRanking: trackA.Ranking,
-				Score:          0,
-			},
-			elo.Elo{
-				CurrentRanking: trackB.Ranking,
-				Score:          1,
-			},
-		)
-
-		fmt.Println(trackA)
-		fmt.Println(trackB)
-		fmt.Println()
+	meta, err := tag.ReadFrom(file)
+	if err != nil {
+		panic(err)
 	}
+	fmt.Printf("%#v\n", meta)
 
-	fmt.Printf("%v\n", tracks)
+	for _, v := range []string{
+		meta.Album(),
+		meta.AlbumArtist(),
+		meta.Artist(),
+		meta.Composer(),
+		meta.Genre(),
+		meta.Title(),
+	} {
+		fmt.Println(v)
+	}
 }
